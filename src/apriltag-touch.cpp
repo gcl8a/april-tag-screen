31f2bf3d668d8e0998b7fc61b9a5144707e9b7e0
Greg Lewin <gcl8a@icloud.com>
2022-10-29 18:34:26 -0400

    added keypad to define tags

--- src/apriltag-touch.cpp
+++ src/apriltag-touch.cpp
@@ -9,6 +9,7 @@
 #include <tag-widget.h>
 #include <tag-library.h>
 #include <button.h>
+#include <keypad.h>
 
 Screen screen;
 
@@ -21,9 +22,50 @@ EventTimer timerDisplay;
 Page* pageBlank = new Page(&screen);
 Page* pageTag = new Page(&screen);
 
-Button button9(9);
-
 Tag tag;
+
+Button button(11);
+
+MetalKeypad keypad;
+String code;
+
+void handleKeyPress(uint8_t key)
+{
+  if(key == POUND)
+  {
+    if(code.length() == 3)
+    {
+      tag.id = code.substring(0, 2).toInt();
+      tag.rotations = code.substring(2).toInt();
+
+      Serial.print(tag.id);
+      Serial.print('\t');
+      Serial.print(tag.rotations);
+      Serial.print('\n');
+
+      screen.SetActivePage(pageTag);
+      screen.CheckRedraw();
+      timerDisplay.Start(DISPLAY_INTERVAL);
+
+    }
+    code = "";
+  }
+
+  else if(key == STAR)
+  {
+      screen.SetActivePage(pageTag);
+      screen.CheckRedraw();
+      timerDisplay.Start(DISPLAY_INTERVAL);
+  }
+
+  else if(key == 10) code += "0";
+
+  else code += String(key);
+
+  Serial.println(code); //for debugging
+
+  
+}
 
 void SetupPages(void)
 {
@@ -35,7 +77,7 @@ void SetupPages(void)
     pageTag->AddWidget(new TagWidget(&screen, 32, 96, 0, 64, &tag));
     pageBlank->AddWidget(new RectangularWidget(&screen, 32, 96, 0, 64, NONE));
     
-    button9.init();
+    button.init();
 
     Serial.println(F("/SetupPages"));
 }
@@ -58,7 +100,7 @@ void setup(void)
 
 void loop() 
 {
-  if(button9.checkButtonPress())
+  if(button.checkButtonPress())
   {
     screen.SetActivePage(pageTag);
     screen.CheckRedraw();
@@ -70,5 +112,8 @@ void loop()
     screen.SetActivePage(pageBlank);
     screen.CheckRedraw();
   }
+
+  uint8_t key = keypad.checkKeypress();
+  if(key) handleKeyPress(key);
 }
 
